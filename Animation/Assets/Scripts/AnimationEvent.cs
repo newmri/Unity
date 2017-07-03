b18d116e57303d37faff80278a7396b3475b5af1
newmri <newmri@naver.com>
2017-07-03 18:15:16 +0900

    Animation

---
+++ Animation/Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour {
+
+	public void EventTest(int i)
+    {
+        Debug.Log("Animation Event Call" + i.ToString());
+    }
+}
